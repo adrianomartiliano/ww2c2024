44b88ea33c5a6ceabebea8ac0f6f65fb785cb3a1
Adriano Martiliano <adrianomartiliano@gmail.com>
2024-01-13 16:46:16 -0300

    Estilho do Form de Sugestão
    - Adicionado os controlados de estados
    - Imagem de fundo
    - Função de envio

--- src/components/Contato.js
+++ src/components/Contato.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./contato.css";
 
 const Contato = () => {
+
+    const [nome, setNome] = useState();
+    const [email, setEmail] = useState();
+    const [recado, setRecado] = useState();
+
+    const handleNome = (e) => {
+        setNome(e.target.value);
+    }
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    const handleRecado = (e) => {
+        setRecado(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+
     return (
-        <>
-            <form action="#" method="POST" className="form_contato">
+        <div className="conteudo">
+            <form onSubmit={handleSubmit} method="POST" className="form_contato">
                 <label className="label_form_contat">
-                    <span for="nome" className="description_span_form">Nome: </span>
-                    <input name="nome" className="input_form_contat"/>
+                    <h2 className="title_form">Envie sua sugestão</h2>
+                </label>
+                <label htmlFor="nome" className="label_form_contat">
+                    <span className="description_span_form">Nome: </span>
+                    <input name="nome" className="input_form_contat" onChange={handleNome}/>
+                </label>
+                <label htmlFor="email" className="label_form_contat">
+                    <span className="description_span_form"> E-mail: </span>
+                    <input name="email" className="input_form_contat" onChange={handleEmail}/>
+                </label>
+                <label htmlFor="recado" className="label_form_contat">
+                    <span className="description_span_form">Recado: </span>
+                    <textarea name="recado" className="input_form_contat" onChange={handleRecado}></textarea>
                 </label>
                 <label className="label_form_contat">
-                    <span for="email" className="description_span_form"> E-mail: </span>
-                    <input name="email" className="input_form_contat"/>
-                </label>
-                <label className="label_form_contat">
-                    <span for="recado" className="description_span_form">Recado: </span>
-                    <textarea name="recado" className="input_form_contat"></textarea>
+                    <button className="btn_form_submit">Enviar</button>
                 </label>
             </form>
-        </>
+        </div>
     )
 }
 
