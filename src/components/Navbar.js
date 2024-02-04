30ec29c7c90a4351d0be1959a73a5eb28ffe14cc
Adriano Martiliano <adrianomartiliano@gmail.com>
2024-02-04 19:33:10 -0300

    Menu Responsivo
    - Adicionado lógica do Menu com telas abaixo de 700px

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,36 @@
 import { Link } from "react-router-dom"
 import "./Navbar.css"
+import { useState } from "react"
 
 const Navbar = () => {
+
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    
+
+    const toggleMenu = () => {
+        setMenuAberto(!menuAberto);
+        console.log(menuAberto)
+    };
+
     return(
         <div className="containerNavBar">
             <div className="logoNavbar">
-                <Link to="/"><img src="../wwc2.png"></img></Link>
+                <Link to="/" onClick={toggleMenu}><img src="../wwc2.png"></img></Link>
                 
             </div>
-            <nav>
-                <Link to="/mural" className="linkmenu">Mural</Link>
+            <div>
+
+            <nav className={`navMenu${menuAberto ? ' aberto' : ''}`}>
+                <Link to="/mural" className="linkmenu" onClick={toggleMenu}>Torneios</Link>
+                <Link to="/" className="linkmenu" onClick={toggleMenu}>Mural</Link>
             </nav>
+            <div className="hamburger" onClick={toggleMenu}>
+                <div className={`line ${menuAberto ? 'aberto' : ''}`}></div>
+                <div className={`line ${menuAberto ? 'aberto' : ''}`}></div>
+                <div className={`line ${menuAberto ? 'aberto' : ''}`}></div>
+            </div>
+            </div>
         </div>
         
     )
