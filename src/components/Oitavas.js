f1b0d30e8653f4ce4d016006338384eee1145674
Adriano Martiliano <adrianomartiliano@gmail.com>
2024-03-05 22:09:20 -0300

    Atualização de resultados
    Atualização de resultados

--- src/components/Oitavas.js
+++ src/components/Oitavas.js
@@ -1,10 +1,14 @@
 import React from "react";
+import "./Eliminatorias.css";
 
-const Oitavas = () =>{
-    return(
-        <div>
+const Oitavas = () => {
+  return (
+    <div className="eliminatorias-container">
+        <div className="eliminatorias-match">
+            <legend>Oitavas 1</legend>
         </div>
-    )
-}
+    </div>
+  );
+};
 
 export default Oitavas;
