a148c0ed2c7eedf2c18d8079ea643d393e17e45e
Adriano Martiliano <adrianomartiliano@gmail.com>
2024-03-18 02:35:52 -0300

    Atualização de Resultados

--- src/components/Oitavas.js
+++ src/components/Oitavas.js
@@ -6,35 +6,35 @@ const Oitavas = () => {
     <div className="eliminatorias-container">
         <h4 className="titleOitavas">Oitavas 1</h4>
         <div className="eliminatorias-match">
-          <span className="jogador">Jogador 1</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 9</span>
+          <span className="jogador">-Sдvдge Mфde-</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Corisco</span>
         </div>
         <h4 className="titleOitavas">Oitavas 2</h4>
         <div className="eliminatorias-match">
-            <span className="jogador">Jogador 2</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 10</span>
+            <span className="jogador">samurai Hyoga</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">KILLER_MOUSTER</span>
         </div>
         <h4 className="titleOitavas">Oitavas 3</h4>
         <div className="eliminatorias-match">
-            <span className="jogador">Jogador 3</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 11</span>
+            <span className="jogador">Lokinho13</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Fábio Ismael</span>
         </div>
         <h4 className="titleOitavas">Oitavas 4</h4>
         <div className="eliminatorias-match">
-            <span className="jogador">Jogador 4</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 12</span>
+            <span className="jogador">Leleco</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">G Ф K Ц</span>
         </div>
         <h4 className="titleOitavas">Oitavas 5</h4>
         <div className="eliminatorias-match">
-            <span className="jogador">Jogador 5</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 13</span>
+            <span className="jogador">-P90-</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">PANTERA NEGRA BR</span>
         </div>
         <h4 className="titleOitavas">Oitavas 6</h4>
         <div className="eliminatorias-match">
-            <span className="jogador">Jogador 6</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 14</span>
+            <span className="jogador">Joleno matador</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">STEVEN</span>
         </div>
         <h4 className="titleOitavas">Oitavas 7</h4>
         <div className="eliminatorias-match">
-            <span className="jogador">Jogador 7</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 15</span>
+            <span className="jogador">Pito Loco</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Rei julian</span>
         </div>
         <h4 className="titleOitavas">Oitavas 8</h4>
         <div className="eliminatorias-match">
-            <span className="jogador">Jogador 8</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">Jogador 16</span>
+            <span className="jogador">-ф-РЭXЭBЯ-ф-</span><span className="placar">0</span>X<span className="placar">0</span><span className="jogador">RM908</span>
         </div>
     </div>
   );
